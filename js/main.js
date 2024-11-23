ad95492cee26c83fa3e77b35b261d9fe3fb9a4fa
danaffn <afanasevda26@gmail.com>
2024-11-23 15:51:28 +0500

    added slider section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 $(function () {
-  
+  const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+    centeredSlides: true,
+    grabCursor: true,
+    slidesPerView: 3.5,
+    mousewheel: true,
+    spaceBetween: 24,
+    slideToClickedSlide: true,
+  });
+
 })
