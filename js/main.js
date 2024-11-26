de8cbeefb94b8f08be8a92a7666a3f9aef0d79a9
danaffn <afanasevda26@gmail.com>
2024-11-26 17:06:41 +0500

    header adaptive

--- js/main.js
+++ js/main.js
@@ -10,4 +10,28 @@ $(function () {
     slideToClickedSlide: true,
   });
 
+  setInterval(() => {
+    if ($(window).scrollTop() > 0 && $('.header__top').hasClass('header__top--open') === false) {
+      $('.burger').addClass('burger--follow')
+    } else {
+      $('.burger').removeClass('burger--follow')
+    }
+  }, 0)
+
+  $('.burger, .overlay').on('click', function (e) {
+    e.preventDefault()
+    $('.header__top').toggleClass('header__top--open')
+    $('.overlay').toggleClass('overlay--show')
+    $('.burger').toggleClass('burger--close')
+  })
+  setInterval(() => {
+    if ($('.header__top').hasClass('header__top--open')) {
+      $('.header__top-nav-list-item, .header__top-img, .header__top-socials-list-item, .header__top-button').on('click', function (e) {
+        $('.header__top').removeClass('header__top--open')
+        $('.overlay').removeClass('overlay--show')
+        $('.burger').removeClass('burger--close')
+      })
+    }
+  }, 0);
+
 })
